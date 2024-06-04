e29eb98e7d77d30fbd780b474ca4cae042185e0c
kk0139317 <kk4881757@gmail.com>
2024-06-04 17:54:12 +0530

    new file:   app/performance/page.tsx 	new file:   components/Performance/index.tsx 	modified:   package-lock.json 	modified:   package.json

---
+++ app/performance/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Performance from '@/components/Performance'
+const page = () => {
+    
+  return (
+    <div>
+      <Performance/>
+    </div>
+  )
+}
+
+export default page
---
+++ components/Performance/index.tsx
@@ -0,0 +1,32 @@
+'use client';
+import React from 'react'
+import { useState } from 'react'
+import {
+    motion,
+    useMotionValue,
+    useTransform,
+} from "framer-motion"
+
+
+const Performance = () => {
+    const x = useMotionValue(0)
+    const background = useTransform(
+        x,
+        [-100, 0, 100],
+        ["#ff008c", "#7700ff", "rgb(230, 255, 0)"]
+    )
+
+  return (
+      <motion.div style={{ background }}>
+          <motion.div
+              drag="x"
+              dragConstraints={{ left: 0, right: 0 }}
+              style={{ x }}
+          >
+              <Icon x={x} />
+          </motion.div>
+      </motion.div>
+  )
+}
+
+export default Performance
